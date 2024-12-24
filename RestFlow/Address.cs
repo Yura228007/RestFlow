7d169be02d016fb0d41851fc70c50228d0ec3579
Yura <iuriishchedrin@gmail.com>
2024-12-25 02:50:16 +0400

    Add class Address

---
+++ RestFlow/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestFlow
+{
+    internal class Address
+    {
+        public string Street { get; set; }
+        public string House { get; set; }     
+        public string Apartment { get; set; }
+        public string Comment { get; set; } 
+
+        public Address(string street, string house, string apartment, string comment)
+        {
+            Street = street;
+            House = house;
+            Apartment = apartment;
+            Comment = comment;
+        }
+
+        public override string ToString()
+        {
+            return $"{Street}, {House}, {Apartment} - {Comment}";
+        }
+    }
+}
