b22ea356afedf3960ffda082bc851c51f4d9f8de
Yura Schedrin <iuriishchedrin@gmail.com>
2025-01-28 18:44:50 +0400

    Revert "Drop Files"
    This reverts commit 303e988e4b856597c0ae1aeb344e3dee609303ca.

---
+++ RestFlow/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestFlow
+{
+    internal class Address
+    {
+        public string Street { get; set; }
+        public string House { get; set; }     
+        public string Apartment { get; set; }
+
+        public Address(string street, string house, string apartment)
+        {
+            Street = street;
+            House = house;
+            Apartment = apartment;
+        }
+
+        public override string ToString()
+        {
+            return $"{Street}, {House}, {Apartment}";
+        }
+    }
+}
