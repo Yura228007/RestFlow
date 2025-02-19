ebcb552d8635ce73cb7a084d6a49d5a91f0e535e
Yura Schedrin <iuriishchedrin@gmail.com>
2025-02-19 04:00:28 +0400

    start work with admin_window

--- RestFlow/Admin_Window.xaml.cs
+++ RestFlow/Admin_Window.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using RestFlow;
 
 namespace RestMenef
 {
@@ -19,6 +21,17 @@ namespace RestMenef
     /// </summary>
     public partial class Admin_Window : Window
     {
+        private class MainViewModel
+        {
+            public ObservableCollection<Employee> Employees { get; set; }
+            public MainViewModel()
+            {
+                Employees = new ObservableCollection<Employee>
+                {
+
+                };
+            }
+        }
         public Admin_Window()
         {
             InitializeComponent();
