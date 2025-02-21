f625bd54b34e8eb5c4d65c6fc2d75efc0111600e
Yura Schedrin <iuriishchedrin@gmail.com>
2025-02-21 05:14:53 +0400

    modificate Employee class

--- RestFlow/Employee.cs
+++ RestFlow/Employee.cs
@@ -1,26 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RestFlow
 {
-    public class Employee : Human
+    public class Employee : Human, INotifyPropertyChanged
     {
-        public double Salary {  get; set; }
-        public string? Post {  get; set; }
+        private double _salary;
+        private string? _post;
 
-        public Employee(string login, string password, string name, string surname, DateTime birthday, bool gender, string phone, double salary)
+        public double Salary
+        {
+            get => _salary;
+            set
+            {
+                _salary = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string? Post
+        {
+            get => _post;
+            set
+            {
+                _post = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Employee(
+            string login,
+            string password,
+            string name,
+            string surname,
+            DateTime birthday,
+            bool gender,
+            string phone,
+            double salary)
             : base(login, password, name, surname, birthday, gender, phone)
         {
             Salary = salary;
         }
 
-        public Employee(DB.Employee employee) : base(employee.Login, employee.Password, employee.Name, employee.Surname, employee.Birthday, employee.Gender, employee.Phone)
+        public Employee(DB.Employee employee)
+            : base(
+                employee.Login,
+                employee.Password,
+                employee.Name,
+                employee.Surname,
+                employee.Birthday,
+                employee.Gender,
+                employee.Phone)
         {
             Salary = employee.Salary;
             Post = employee.Post;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
